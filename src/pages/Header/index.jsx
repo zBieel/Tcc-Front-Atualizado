082232cd62e9b697e54952bdc8b7c124c97c4784
Gabrielnunes8 <gabriel.feitosa@gicbrasil.com>
2024-09-24 20:55:16 -0300

    aaa

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -5,7 +5,7 @@ function Header(){
 
     return(
             <header>
-                <a href="/" className="logo">RESERVAKI</a>
+                <a href="/home" className="logo">RESERVAKI</a>
                 <a href="/usuario" className="usuario">Cadastrar Usuário</a>
                 <a href="/agendarmesas" className="usuario">Agendar Mesas</a>
             </header>
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,22 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Login from './pages/Login';
+import Header from './pages/Header';
 import Home from './pages/Home';
+import Usuario from './pages/Usuario';
+import AgendarMesas from './pages/AgendarMesas';
+
 
 
 function RoutesApp(){
  return(
     <BrowserRouter>
+        <Header />
             <Routes>
                     <Route path="/" element={<Login/>}/>
                     <Route path="/home" element={<Home/>}/>
+                    <Route path="/usuario" element={<Usuario/>}/>
+                    <Route path="/agendarmesas" element={<AgendarMesas/>}/>
             </Routes>
     </BrowserRouter>
  )
